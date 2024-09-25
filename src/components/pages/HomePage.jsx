e8b72e8ab61913589801c301335bb261c9d2173d
Гэрэлтбаатар Мягмар <24LP8388@G401-CC0119.local>
2024-09-25 18:55:52 +0800

    ehnii odor

---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import Header from "../patrs/Header";
+
+const HomePage = () => {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+};
+
+export default HomePage;
---
+++ src/components/patrs/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <main>
+      <div className="h-screen border flex flex-col justify-end items-end">
+        <div className="flex items-center justify-center border border-black rounded-[50%] w-14 h-14 hover:rounded-[27px]  hover:w-full duration-500">
+          minu
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Header;
